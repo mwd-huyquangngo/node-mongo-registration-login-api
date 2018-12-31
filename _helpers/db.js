2e51ce0ef450c7c7fed011ba9b6e9ee9fc1cbc2a
mwd-huyquangngo <huy.quang.ngo@mowede.com>
2018-12-31 22:40:18 +0700

    Add ESLint into project

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,8 +1,8 @@
-const config = require('config.json');
+const config = require('../config.json');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || config.connectionString);
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model')
+  User: require('../users/user.model')
 };
